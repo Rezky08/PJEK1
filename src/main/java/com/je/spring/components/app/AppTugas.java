3e720498e1ff005c40bef2f64b5c93cc4ed53b9a
rezky setiawan <rezky.setiawan85@gmail.com>
2020-03-30 09:23:41 +0700

    change insert service

--- src/main/java/com/je/spring/components/app/AppTugas.java
+++ src/main/java/com/je/spring/components/app/AppTugas.java
@@ -14,7 +14,6 @@ import com.je.spring.components.service.EmployeeService;
 import com.je.spring.components.service.MahasiswaService;
 import com.je.spring.components.service.MatakuliahService207225;
 
-
 public class AppTugas {
 
     public static void main(String[] args) {
@@ -28,13 +27,13 @@ public class AppTugas {
         System.out.println("Start Insert Customer");
         customerService.insertCustomer();
         System.out.println("Finish Insert Customer");
-        
+
         System.out.println("Customer id: " + customer.getId());
         System.out.println("Customer Nama: " + customer.getNama());
         System.out.println("Customer Alamat: " + customer.getAlamat());
-        
+
         System.out.println("-------------------------");
-        
+
         MahasiswaService mahasiswaService = (MahasiswaService) context.getBean("mahasiswaService");
         mahasiswaService.insertMahasiswa();
         Mahasiswa mahasiswa = new Mahasiswa();
@@ -42,15 +41,15 @@ public class AppTugas {
         mahasiswa.setNama("Ravi Melan");
         mahasiswa.setAlamat("Sarana Indah Permai");
         System.out.println("Start Insert Mahasiswa");
-        customerService.insertCustomer();
+        mahasiswaService.insertMahasiswa();
         System.out.println("Finish Insert Mahasiswa");
 
         System.out.println("Mahasiswa id: " + mahasiswa.getId());
         System.out.println("Mahasiswa Nama: " + mahasiswa.getNama());
         System.out.println("Mahasiswa Alamat: " + mahasiswa.getAlamat());
-        
+
         System.out.println("-------------------------");
-        
+
         EmployeeService employeeService = (EmployeeService) context.getBean("employeeService");
         employeeService.insertEmployee();
         Employee employee = new Employee();
@@ -58,15 +57,15 @@ public class AppTugas {
         employee.setNama("Ravi Melan");
         employee.setAlamat("Sarana Indah Permai");
         System.out.println("Start Insert Employee");
-        customerService.insertCustomer();
+        employeeService.insertEmployee();
         System.out.println("Finish Insert Employee");
 
         System.out.println("Employee id: " + employee.getId());
         System.out.println("Employee Nama: " + employee.getNama());
         System.out.println("Employee Alamat: " + employee.getAlamat());
 
-                System.out.println("-------------------------");
-        
+        System.out.println("-------------------------");
+
         MatakuliahService207225 matakuliahService207225 = (MatakuliahService207225) context.getBean("mataKuliahService");
         matakuliahService207225.insertMatakuliah();
         Matakuliah207225 mataKuliah = new Matakuliah207225();
