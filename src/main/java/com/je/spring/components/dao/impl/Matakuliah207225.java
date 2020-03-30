f2527303e41652ffb207cb191a72d2c8b270afd6
fekuna <hichirro@gmail.com>
2020-03-30 09:27:22 +0700

    Penambahan Dao dan Implement Matakuliah

---
+++ src/main/java/com/je/spring/components/dao/impl/Matakuliah207225.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.je.spring.components.dao.impl;
+
+import com.je.spring.components.dao.Matakuliah207225;
+import org.springframework.stereotype.Repository;
+
+@Repository("matakuliahDao")
+
+/**
+ *
+ * @author hichi
+ */
+public class Matakuliah207225 implements Matakuliah207225{
+     public void insertMatakuliah207225() {
+        System.out.println("INSERT MataKuliah");
+    }
+}
