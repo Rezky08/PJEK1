43a006d4e0a8abcd5a1a0cb2270ad4921ac4e2b7
rezky setiawan <rezky.setiawan85@gmail.com>
2020-03-30 08:52:47 +0700

    Add App Tugas and Model Matkul

---
+++ src/main/java/com/je/spring/components/app/AppTugas.java
@@ -0,0 +1,82 @@
+/*
+ @author Ravi Melan
+ */
+package com.je.spring.components.app;
+
+import com.je.spring.components.model.Customer;
+import com.je.spring.components.model.Employee;
+import com.je.spring.components.model.Mahasiswa;
+import com.je.spring.components.model.Matakuliah;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.je.spring.components.service.CustomerService;
+import com.je.spring.components.service.EmployeeService;
+import com.je.spring.components.service.MahasiswaService;
+import com.je.spring.components.service.MatakuliahService;
+
+
+public class AppTugas {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring-config.xml"});
+
+        CustomerService customerService = (CustomerService) context.getBean("customerService");
+        Customer customer = new Customer();
+        customer.setId(1);;
+        customer.setNama("Ravi Melan");
+        customer.setAlamat("Sarana Indah Permai");
+        System.out.println("Start Insert Customer");
+        customerService.insertCustomer();
+        System.out.println("Finish Insert Customer");
+        
+        System.out.println("Customer id: " + customer.getId());
+        System.out.println("Customer Nama: " + customer.getNama());
+        System.out.println("Customer Alamat: " + customer.getAlamat());
+        
+        System.out.println("-------------------------");
+        
+        MahasiswaService mahasiswaService = (MahasiswaService) context.getBean("mahasiswaService");
+        mahasiswaService.insertMahasiswa();
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.setId(1);;
+        mahasiswa.setNama("Ravi Melan");
+        mahasiswa.setAlamat("Sarana Indah Permai");
+        System.out.println("Start Insert Mahasiswa");
+        customerService.insertCustomer();
+        System.out.println("Finish Insert Mahasiswa");
+
+        System.out.println("Mahasiswa id: " + mahasiswa.getId());
+        System.out.println("Mahasiswa Nama: " + mahasiswa.getNama());
+        System.out.println("Mahasiswa Alamat: " + mahasiswa.getAlamat());
+        
+        System.out.println("-------------------------");
+        
+        EmployeeService employeeService = (EmployeeService) context.getBean("employeeService");
+        employeeService.insertEmployee();
+        Employee employee = new Employee();
+        employee.setId(1);;
+        employee.setNama("Ravi Melan");
+        employee.setAlamat("Sarana Indah Permai");
+        System.out.println("Start Insert Employee");
+        customerService.insertCustomer();
+        System.out.println("Finish Insert Employee");
+
+        System.out.println("Employee id: " + employee.getId());
+        System.out.println("Employee Nama: " + employee.getNama());
+        System.out.println("Employee Alamat: " + employee.getAlamat());
+
+                System.out.println("-------------------------");
+        
+        MatakuliahService mataKuliahService = (MatakuliahService) context.getBean("mataKuliahService");
+        mataKuliahService.insertMataKuliah();
+        Matakuliah mataKuliah = new Matakuliah();
+        mataKuliah.setKodeMatkul207225(1);;
+        mataKuliah.setNamaMatkul207225("Java Enterprise");
+        System.out.println("Start Insert Mata Kuliah");
+        mataKuliahService.insertMataKuliah();
+        System.out.println("Finish Insert Mata Kuliah");
+
+        System.out.println("Kode Mata Kuliah: " + mataKuliah.getKodeMatkul207225());
+        System.out.println("Nama Mata Kuliah: " + mataKuliah.getNamaMatkul207225());
+    }
+}
---
+++ src/main/java/com/je/spring/components/model/Matakuliah.java
@@ -0,0 +1,27 @@
+/*
+ @author Ravi Melan
+ */
+package com.je.spring.components.model;
+
+public class Matakuliah {
+
+    private int kodeMatkul207225;
+    private String namaMatkul207225;
+
+    public int getKodeMatkul207225() {
+        return kodeMatkul207225;
+    }
+
+    public void setKodeMatkul207225(int kodeMatkul207225) {
+        this.kodeMatkul207225 = kodeMatkul207225;
+    }
+
+    public String getNamaMatkul207225() {
+        return namaMatkul207225;
+    }
+
+    public void setNamaMatkul207225(String namaMatkul207225) {
+        this.namaMatkul207225 = namaMatkul207225;
+    }
+
+}
